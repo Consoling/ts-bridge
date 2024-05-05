39e0c3545ba275b066b68dff4527d78205331161
Consoling <shahidarizfannymagnet@gmail.com>
2024-05-05 21:49:22 +0530

    modified courses page and uploaded imgs for each courses

--- js/master.js
+++ js/master.js
@@ -82,94 +82,11 @@ const cards = [
   },
 ];
 
-// if(window.location.pathname === '/' || '/index.html') {
-//   const exploreProgCards = document.getElementById("exploreProgCards"); //Card container
-
-// cards.forEach((card, index) => {
-//   //Card Mapping
-//   const exploreProgCard = document.createElement("div");
-//   exploreProgCard.className =
-//     "explore_prog_card items-center modal-open transition-all duration-300 ease-out";
-//   exploreProgCard.setAttribute("data-modal", `modal-${index + 1}`);
-
-//   const img = document.createElement("img");
-//   img.className = "w-15 h-20";
-//   img.src = `/media/image.jpg`;
-//   img.alt = "";
-
-//   const title = document.createElement("h3");
-//   title.className = "text-white font-bold mt-6 text-lg";
-//   title.textContent = card.title;
-
-//   const description = document.createElement("p");
-//   description.className = "text-white font-semibold text-sm";
-//   description.textContent = "See Program->";
-
-//   exploreProgCard.appendChild(img);
-//   exploreProgCard.appendChild(title);
-//   exploreProgCard.appendChild(description);
-
-//   const modal = document.createElement("div");
-//   modal.className =
-//     "modal shadow-md shadow-black/90 ring-1 hidden transition-all duration-300 ease-out";
-//   modal.id = `modal-${index + 1}`;
-
-//   const modalHeading = document.createElement("div");
-//   modalHeading.className = "modal-heading";
-
-//   const modalTitle = document.createElement("h1");
-//   modalTitle.id = `modal-title-${index + 1}`;
-//   modalTitle.textContent = card.title;
-
-//   const modalClose = document.createElement("i");
-//   modalClose.className = "fa-solid fa-xmark cursor-pointer modal-close";
-
-//   const hr = document.createElement("hr");
-
-//   const modalContent = document.createElement("div");
-//   modalContent.className = "modal-content";
-
-//   const modalDescription = document.createElement("h1");
-//   modalDescription.id = `modal-description-${index + 1}`;
-//   modalDescription.textContent = card.description;
-
-//   const ul = document.createElement("ul");
-
-//   card.features.forEach((feature, i) => {
-//     const li = document.createElement("li");
-//     li.id = `modal-feature${i + 1}-${index + 1}`;
-//     li.textContent = feature;
-//     ul.appendChild(li);
-//   });
-
-//   modalContent.appendChild(modalDescription);
-//   modalContent.appendChild(ul);
-
-//   const button = document.createElement("button");
-//   const a = document.createElement("a");
-//   a.href = "/courses.html";
-//   a.className = "underline-none";
-//   a.textContent = "See Details ";
-//   const i = document.createElement("i");
-//   i.className = "fa-solid fa-arrow-up-right-from-square";
-//   a.appendChild(i);
-//   button.appendChild(a);
-
-//   modal.appendChild(modalHeading);
-//   modalHeading.appendChild(modalTitle);
-//   modalHeading.appendChild(modalClose);
-//   modal.appendChild(hr);
-//   modal.appendChild(modalContent);
-//   modal.appendChild(button);
-
-//     exploreProgCards.appendChild(exploreProgCard);
-//     exploreProgCards.appendChild(modal);
-
-// });
-// }
-
 document.addEventListener("DOMContentLoaded", () => {
-  if (window.location.pathname === "/" || "/index.html") {
+  if (
+    window.location.pathname === "/" ||
+    window.location.pathname === "/index.html"
+  ) {
     const exploreProgCards = document.getElementById("exploreProgCards"); // Card container
 
     cards.forEach((card, index) => {
@@ -369,119 +286,126 @@ const mobileFaq = document.getElementById("m-answer-faq");
 const mobileAnswerForm = document.querySelector(".mobile-answer-form");
 const mobileFeedbackForm = document.querySelector(".mobile-feedback-form");
 
-q1.addEventListener("click", () => {
-  answerFaq.innerHTML = `TS Bridge Edu is a community where you can learn skills, network with smart people, and find work.
+if (
+  window.location.pathname === "/" ||
+  window.location.pathname === "/index.html"
+) {
+  q1.addEventListener("click", () => {
+    answerFaq.innerHTML = `TS Bridge Edu is a community where you can learn skills, network with smart people, and find work.
   Join the WhatsApp Community to find all these opportunities.`;
-  if (!feedbackForm.classList.contains("hidden")) {
-    feedbackForm.classList.add("hidden");
-  }
-  if (feedbackForm.classList.contains("hidden")) {
-    answerForm.classList.remove("hidden");
-  }
-});
-
-mq1.addEventListener("click", () => {
-  mobileFaq.innerHTML = `TS Bridge Edu is a community where you can learn skills, network with smart people, and find work.
-  Join the WhatsApp Community to find all these opportunities.`;
-  if (!mobileFeedbackForm.classList.contains("hidden")) {
-    mobileFeedbackForm.classList.add("hidden");
-  }
-
-  if (mobileFeedbackForm.classList.contains("hidden")) {
-    mobileAnswerForm.classList.remove("hidden");
-  }
-});
-
-q2.addEventListener("click", () => {
-  answerFaq.innerHTML = `No TS Bridge Edu is completely free.`;
-  if (!feedbackForm.classList.contains("hidden")) {
-    feedbackForm.classList.add("hidden");
-  }
-
-  if (feedbackForm.classList.contains("hidden")) {
-    answerForm.classList.remove("hidden");
-  }
-});
-
-mq2.addEventListener("click", () => {
-  mobileFaq.innerHTML = `No TS Bridge Edu is completely free.`;
-
-  if (!mobileFeedbackForm.classList.contains("hidden")) {
-    mobileFeedbackForm.classList.add("hidden");
-  }
-
-  if (mobileFeedbackForm.classList.contains("hidden")) {
-    mobileAnswerForm.classList.remove("hidden");
-  }
-});
-
-q3.addEventListener("click", () => {
-  answerFaq.innerHTML = `It's simple. Join our WhatsApp Community. Fill the internship form as per your skills and interest. Have a screening session. Congrats!`;
-  if (!feedbackForm.classList.contains("hidden")) {
-    feedbackForm.classList.add("hidden");
-  }
-
-  if (feedbackForm.classList.contains("hidden")) {
-    answerForm.classList.remove("hidden");
-  }
-});
-
-mq3.addEventListener("click", () => {
-  mobileFaq.innerHTML = `It's simple. Join our WhatsApp Community. Fill the internship form as per your skills and interest. Have a screening session. Congrats!`;
-  if (!mobileFeedbackForm.classList.contains("hidden")) {
-    mobileFeedbackForm.classList.add("hidden");
-  }
-
-  if (mobileFeedbackForm.classList.contains("hidden")) {
-    mobileAnswerForm.classList.remove("hidden");
-  }
-});
-
-q4.addEventListener("click", () => {
-  answerFaq.innerHTML = `It's simple. Fill the requirement form.`;
-  if (!feedbackForm.classList.contains("hidden")) {
-    feedbackForm.classList.add("hidden");
-  }
-
-  if (feedbackForm.classList.contains("hidden")) {
-    answerForm.classList.remove("hidden");
-  }
-});
-
-mq4.addEventListener("click", () => {
-  mobileFaq.innerHTML = `It's simple. Fill the requirement form.`;
-
-  if (!mobileFeedbackForm.classList.contains("hidden")) {
-    mobileFeedbackForm.classList.add("hidden");
-  }
-
-  if (mobileFeedbackForm.classList.contains("hidden")) {
-    mobileAnswerForm.classList.remove("hidden");
-  }
-});
-
-document.getElementById("q5").addEventListener("click", function () {
-  feedbackForm.classList.toggle("hidden");
-  if (!feedbackForm.classList.contains("hidden")) {
-    answerForm.classList.toggle("hidden");
-  }
-});
-
-document.getElementById("mq5").addEventListener("click", function () {
-  mobileFeedbackForm.classList.toggle("hidden");
-  if (!mobileFeedbackForm.classList.contains("hidden")) {
-    mobileAnswerForm.classList.toggle("hidden");
-  }
-});
-
-faqQuestions.forEach((question) => {
-  question.addEventListener("click", () => {
-    faqQuestions.forEach((q) => q.classList.remove("active"));
-    question.classList.add("active");
+    if (!feedbackForm.classList.contains("hidden")) {
+      feedbackForm.classList.add("hidden");
+    }
+    if (feedbackForm.classList.contains("hidden")) {
+      answerForm.classList.remove("hidden");
+    }
   });
-});
 
-if (window.location.pathname === "/" || "index.html") {
+  mq1.addEventListener("click", () => {
+    mobileFaq.innerHTML = `TS Bridge Edu is a community where you can learn skills, network with smart people, and find work.
+  Join the WhatsApp Community to find all these opportunities.`;
+    if (!mobileFeedbackForm.classList.contains("hidden")) {
+      mobileFeedbackForm.classList.add("hidden");
+    }
+
+    if (mobileFeedbackForm.classList.contains("hidden")) {
+      mobileAnswerForm.classList.remove("hidden");
+    }
+  });
+
+  q2.addEventListener("click", () => {
+    answerFaq.innerHTML = `No TS Bridge Edu is completely free.`;
+    if (!feedbackForm.classList.contains("hidden")) {
+      feedbackForm.classList.add("hidden");
+    }
+
+    if (feedbackForm.classList.contains("hidden")) {
+      answerForm.classList.remove("hidden");
+    }
+  });
+
+  mq2.addEventListener("click", () => {
+    mobileFaq.innerHTML = `No TS Bridge Edu is completely free.`;
+
+    if (!mobileFeedbackForm.classList.contains("hidden")) {
+      mobileFeedbackForm.classList.add("hidden");
+    }
+
+    if (mobileFeedbackForm.classList.contains("hidden")) {
+      mobileAnswerForm.classList.remove("hidden");
+    }
+  });
+
+  q3.addEventListener("click", () => {
+    answerFaq.innerHTML = `It's simple. Join our WhatsApp Community. Fill the internship form as per your skills and interest. Have a screening session. Congrats!`;
+    if (!feedbackForm.classList.contains("hidden")) {
+      feedbackForm.classList.add("hidden");
+    }
+
+    if (feedbackForm.classList.contains("hidden")) {
+      answerForm.classList.remove("hidden");
+    }
+  });
+
+  mq3.addEventListener("click", () => {
+    mobileFaq.innerHTML = `It's simple. Join our WhatsApp Community. Fill the internship form as per your skills and interest. Have a screening session. Congrats!`;
+    if (!mobileFeedbackForm.classList.contains("hidden")) {
+      mobileFeedbackForm.classList.add("hidden");
+    }
+
+    if (mobileFeedbackForm.classList.contains("hidden")) {
+      mobileAnswerForm.classList.remove("hidden");
+    }
+  });
+
+  q4.addEventListener("click", () => {
+    answerFaq.innerHTML = `It's simple. Fill the requirement form.`;
+    if (!feedbackForm.classList.contains("hidden")) {
+      feedbackForm.classList.add("hidden");
+    }
+
+    if (feedbackForm.classList.contains("hidden")) {
+      answerForm.classList.remove("hidden");
+    }
+  });
+
+  mq4.addEventListener("click", () => {
+    mobileFaq.innerHTML = `It's simple. Fill the requirement form.`;
+
+    if (!mobileFeedbackForm.classList.contains("hidden")) {
+      mobileFeedbackForm.classList.add("hidden");
+    }
+
+    if (mobileFeedbackForm.classList.contains("hidden")) {
+      mobileAnswerForm.classList.remove("hidden");
+    }
+  });
+
+  document.getElementById("q5").addEventListener("click", function () {
+    feedbackForm.classList.toggle("hidden");
+    if (!feedbackForm.classList.contains("hidden")) {
+      answerForm.classList.toggle("hidden");
+    }
+  });
+
+  document.getElementById("mq5").addEventListener("click", function () {
+    mobileFeedbackForm.classList.toggle("hidden");
+    if (!mobileFeedbackForm.classList.contains("hidden")) {
+      mobileAnswerForm.classList.toggle("hidden");
+    }
+  });
+
+  faqQuestions.forEach((question) => {
+    question.addEventListener("click", () => {
+      faqQuestions.forEach((q) => q.classList.remove("active"));
+      question.classList.add("active");
+    });
+  });
+}
+if (
+  window.location.pathname === "/" ||
+  window.location.pathname === "/index.html"
+) {
   var counterTriggered = false;
 
   window.addEventListener("scroll", () => {
@@ -499,23 +423,200 @@ if (window.location.pathname === "/" || "index.html") {
     var counters = document.querySelectorAll('[id^="stats_cntr_"]');
     var targets = [50, 1000, 30, 70]; // Specify the target values here
     var speed = 200; // Speed of counting in milliseconds
-  
-    counters.forEach(function(counter, index) {
+
+    counters.forEach(function (counter, index) {
       var target = targets[index];
       var count = 0;
       var interval = target / speed;
-  
-      var updateCount = function() {
+
+      var updateCount = function () {
         count += interval;
         counter.innerText = Math.floor(count);
         if (count < target) {
           setTimeout(updateCount, 1);
         } else {
-          counter.innerText = target + '+';
+          counter.innerText = target + "+";
         }
       };
-  
+
       updateCount();
     });
   };
 }
+
+if (
+  !window.location.pathname === "/about" ||
+  "/" ||
+  "/courses" ||
+  "/about.html" ||
+  "/courses.html" ||
+  "/index.html"
+) {
+  window.location.href === "404.html";
+}
+
+const projectsData = [
+  {
+    title: "Design a logo and brand identity for a new coffee shop",
+    skills: ["Graphic Design", "Illustrator", "Canva", "Logo Design"],
+    img: "/media/courses-1.png",
+  },
+  {
+    title: "Create a responsive website for a local bakery",
+    skills: ["HTML", "CSS", "JavaScript", "Bootstrap"],
+    img: "/media/courses-2.png",
+  },
+  {
+    title: "Develop a mobile app for tracking fitness activities",
+    skills: [
+      "Mobile App Development",
+      "UI/UX Design",
+      "Firebase",
+      "React Native",
+    ],
+    img: "/media/courses-3.png",
+  },
+  {
+    title: "Design promotional materials for a charity event",
+    skills: ["Graphic Design", "Photoshop", "InDesign", "Print Design"],
+    img: "/media/courses-4.png",
+  },
+  {
+    title: "Build an e-commerce platform for handmade crafts",
+    skills: [
+      "Web Development",
+      "Shopify",
+      "WooCommerce",
+      "Payment Integration",
+    ],
+    img: "/media/courses-5.png",
+  },
+  {
+    title: "Create a social media marketing campaign for a new product launch",
+    skills: [
+      "Digital Marketing",
+      "Social Media Management",
+      "Content Creation",
+      "Analytics",
+    ],
+    img: "/media/courses-6.png",
+  },
+  {
+    title: "Design an infographic for a research report",
+    skills: [
+      "Graphic Design",
+      "Data Visualization",
+      "Illustrator",
+      "Information Design",
+    ],
+    img: "/media/courses-7.png",
+  },
+  {
+    title: "Develop a chatbot for customer support",
+    skills: [
+      "Chatbot Development",
+      "Natural Language Processing",
+      "API Integration",
+      "Python",
+    ],
+    img: "/media/courses-8.png",
+  },
+  {
+    title: "Produce a video tutorial series for beginner programmers",
+    skills: [
+      "Video Production",
+      "Scriptwriting",
+      "Screen Recording",
+      "Video Editing",
+    ],
+    img: "/media/courses-9.png",
+  },
+  {
+    title: "Design a user interface for a car rental app",
+    skills: ["UI/UX Design", "Prototyping", "Figma", "User Research"],
+    img: "/media/courses-10.png",
+  },
+  {
+    title: "Create a podcast series on mindfulness and meditation",
+    skills: [
+      "Audio Production",
+      "Scripting",
+      "Voice Recording",
+      "Podcast Hosting",
+    ],
+    img: "/media/courses-11.png",
+  },
+  {
+    title: "Develop a gamified language learning app",
+    skills: ["Game Development", "Language Learning", "Unity", "Gamification"],
+    img: "/media/courses-12.png",
+  },
+  {
+    title: "Design a set of icons for a productivity app",
+    skills: ["Icon Design", "Illustrator", "Visual Design", "Iconography"],
+    img: "/media/courses-13.png",
+  },
+  {
+    title: "Build a portfolio website for a freelance photographer",
+    skills: [
+      "Web Development",
+      "WordPress",
+      "Responsive Design",
+      "Portfolio Showcase",
+    ],
+    img: "/media/courses-14.png",
+  },
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+  function generateProjectCards() {
+    const container = document.getElementById("projectContainer");
+
+    projectsData.forEach((project) => {
+      const card = document.createElement("div");
+      card.classList.add("project_box", "courses");
+
+     
+
+      const img = document.createElement("img");
+      img.className = "w-full h-32 object-contain ";
+      img.src = project.img;
+      img.alt = "course-ts-bridge";
+
+      const titleElement = document.createElement("h3");
+      titleElement.textContent = project.title;
+
+      const skillsElement = document.createElement("div");
+      skillsElement.classList.add("courses_skills");
+      project.skills.forEach((skill) => {
+        const skillButton = document.createElement("button");
+        skillButton.className = 'bg-gradient-to-r from-violet-600 to-indigo-600 text-white'
+        skillButton.textContent = skill;
+        skillsElement.appendChild(skillButton);
+      });
+
+      const skillsHeading = document.createElement("p");
+      skillsHeading.className = "text-sm font-semibold ";
+      skillsHeading.textContent = "Skills you'll be learning :";
+
+      const button = document.createElement("button");
+      button.className = "button-with-anchor";
+      const anchor = document.createElement("a");
+      anchor.href = 'https://forms.gle/wqSSiMgVUvZd9gKg7';
+      anchor.textContent = "View Details";
+      button.appendChild(anchor);
+
+      card.appendChild(img);
+      card.appendChild(titleElement);
+      card.appendChild(skillsHeading);
+      card.appendChild(skillsElement);
+      card.appendChild(button);
+
+      
+
+      container.appendChild(card);
+    });
+  }
+
+  generateProjectCards();
+});
