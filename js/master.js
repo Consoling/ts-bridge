fcc621c5cc1b6cf08d7bd383e9a12342d72cedd4
singh-tanyaa <96328928+singh-tanyaa@users.noreply.github.com>
2024-05-18 19:33:50 +0530

    added sidenavbar

--- js/master.js
+++ js/master.js
@@ -797,3 +797,37 @@ document.addEventListener("DOMContentLoaded", () => {
 
   generateProjectCards();
 });
+//JAVASCRIPT CODE FOR HAMBURGER
+const body = document.querySelector('body'),
+sidebar = body.querySelector('nav'),
+toggle = body.querySelector(".toggle"),
+searchBtn = body.querySelector(".search-box"),
+modeSwitch = body.querySelector(".toggle-switch"),
+modeText = body.querySelector(".mode-text");
+
+
+toggle.addEventListener("click" , () =>{
+sidebar.classList.toggle("close");
+})
+
+searchBtn.addEventListener("click" , () =>{
+sidebar.classList.remove("close");
+})
+
+modeSwitch.addEventListener("click" , () =>{
+body.classList.toggle("dark");
+
+if(body.classList.contains("dark")){
+  modeText.innerText = "Light mode";
+}else{
+  modeText.innerText = "Dark mode";
+  
+}
+});
+
+
+
+
+
+
+
